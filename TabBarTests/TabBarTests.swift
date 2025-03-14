a70bf93b3681ad10b2a723e361666d30dcfe50b1
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-14 18:38:55 +0300

    Initial Commit

---
+++ TabBarTests/TabBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarTests.swift
+//  TabBarTests
+//
+//  Created by Ömer on 14.03.2025.
+//
+
+import Testing
+@testable import TabBar
+
+struct TabBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
